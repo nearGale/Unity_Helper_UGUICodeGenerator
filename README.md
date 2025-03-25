0f88b74d7fdbdfee4ffa7805e3e83a05c2290338
重剑无锋 <xuuold@gmail.com>
2025-03-25 17:24:23 +0800

    UGUI绑定代码自动生成

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Helper_UGUICodeGenerator
+ UGUI组件自动生成代码
---
+++ Unity_Helper_UGUICodeGenerator/Assets/UGUICodeGenerator/AutoGenCodes/UICode_PanelBg.cs
@@ -0,0 +1,31 @@
+// 自动生成代码，勿手动修改！
+// 生成时间：2025/3/25 16:50:30
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICode_PanelBg : MonoBehaviour
+{
+	[SerializeField] RectTransform rectTransform_ButtonLegacy;
+	[SerializeField] Button button_BtnTest;
+	[SerializeField] Image image_ButtonLegacy;
+	[SerializeField] Text text_TextLegacy;
+	[SerializeField] Transform transform_Image;
+
+
+
+
+	#region 用于寻找控件,当控件丢失,点击脚本齿轮->Reset菜单可恢复,也可重新编写下面的路径代码
+#if UNITY_EDITOR
+	private void Reset()
+	{
+		rectTransform_ButtonLegacy = transform.Find("Button (Legacy)").GetComponent<RectTransform>();
+		button_BtnTest = transform.Find("Button (Legacy)").GetComponent<Button>();
+		image_ButtonLegacy = transform.Find("Button (Legacy)").GetComponent<Image>();
+		text_TextLegacy = transform.Find("Button (Legacy)/Text (Legacy)").GetComponent<Text>();
+		transform_Image = transform.Find("Image").GetComponent<Transform>();
+	}
+#endif
+	#endregion
+}
